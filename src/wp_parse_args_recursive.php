13d3d783812cb2c3a762d6a5e39c56bfc9ecfaca
kallookoo <sergio@dsergio.com>
2017-09-03 10:59:30 +0200

    Add missing variable

--- src/wp_parse_args_recursive.php
+++ src/wp_parse_args_recursive.php
@@ -6,10 +6,10 @@
  *
  * @author Sergio (kallookoo) <sergio@dsergio.com>
  * @license https://www.gnu.org/licenses/gpl-2.0.html GPL2 or later
- * @version 1.0
+ * @version 1.1
  *
  * @param  array|object  $args                   Values to merge with $defaults
- * @param  array|object  $defaults               Array or Object that serves as the defaults.
+ * @param  array|object  $defaults               Array, Object that serves as the defaults or string.
  * @param  boolean       $preserve_type          Optional. Convert output array into object if $args or $defaults if it is. Default true
  * @param  boolean       $preserve_integer_keys  Optional. If given, integer keys will be preserved and merged instead of appended. Default false.
  *
@@ -18,7 +18,6 @@
 if ( ! function_exists( 'wp_parse_args_recursive' ) ) {
 	function wp_parse_args_recursive( $args, $defaults, $preserve_type = true, $preserve_integer_keys = false ) {
 		$output = array();
-
 		foreach ( array( $defaults, $args ) as $list ) {
 			foreach ( (array) $list as $key => $value ) {
 				if ( is_integer( $key ) && ! $preserve_integer_keys ) {
@@ -28,13 +27,12 @@ if ( ! function_exists( 'wp_parse_args_recursive' ) ) {
 					( is_array( $output[ $key ] ) || is_object( $output[ $key ] ) ) &&
 					( is_array( $value ) || is_object( $value ) )
 				) {
-					$output[ $key ] = wp_parse_args_recursive( $value, $output[ $key ], $preserve_integer_keys );
+					$output[ $key ] = wp_parse_args_recursive( $value, $output[ $key ], $preserve_type, $preserve_integer_keys );
 				} else {
 					$output[ $key ] = $value;
 				}
 			}
 		}
-
 		return ( $preserve_type && ( is_object( $args ) || is_object( $defaults ) ) ) ? (object) $output : $output;
 	}
 }
