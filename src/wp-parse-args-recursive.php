7a828eaf9ccc94dac335acac343f06937643884d
kallookoo <kallookoo@users.noreply.github.com>
2022-11-16 11:09:24 +0100

    Added using $args as string.

--- src/wp-parse-args-recursive.php
+++ src/wp-parse-args-recursive.php
@@ -1,11 +1,11 @@
-<?php
+<?php // phpcs:disable WooCommerce.Commenting.CommentTags
 /**
  * Like wp_parse_args but supports recursivity
  * By default converts the returned type based on the $args and $defaults
  *
- * @author Sergio (kallookoo) <sergio@dsergio.com>
+ * @author Sergio (kallookoo) <kallookoo@gmail.com>
  * @license https://www.gnu.org/licenses/gpl-2.0.html GPL2 or later
- * @version 1.2
+ * @version 1.3.0
  *
  * @package dsergio\com\WordPress\Helpers
  */
@@ -15,17 +15,34 @@ if ( ! function_exists( 'wp_parse_args_recursive' ) ) {
 	 * Like wp_parse_args but supports recursivity
 	 * By default converts the returned type based on the $args and $defaults
 	 *
-	 * @param  array|object $args                   Values to merge with $defaults.
-	 * @param  array|object $defaults               Array, Object that serves as the defaults or string.
-	 * @param  boolean      $preserve_type          Optional. Convert output array into object if $args or $defaults if it is. Default true.
-	 * @param  boolean      $preserve_integer_keys  Optional. If given, integer keys will be preserved and merged instead of appended.
+	 * @param  string|array|object $args                   Values to merge with $defaults.
+	 * @param  array|object        $defaults               Optional. Array or Object that serves as the defaults.
+	 *                                                     Default empty array.
+	 * @param  boolean             $preserve_type          Optional. Convert output array into object if $args or $defaults if it is.
+	 *                                                     Default true.
+	 * @param  boolean             $preserve_integer_keys  Optional. If given, integer keys will be preserved and merged instead of appended.
+	 *                                                     Default false.
 	 *
-	 * @return array|object  $output                 Merged user defined values with defaults.
+	 * @return array|object $output Merged user defined values with defaults.
 	 */
-	function wp_parse_args_recursive( $args, $defaults, $preserve_type = true, $preserve_integer_keys = false ) {
+	function wp_parse_args_recursive( $args, $defaults = array(), $preserve_type = true, $preserve_integer_keys = false ) {
 		$output = array();
+		if ( is_string( $args ) ) {
+			parse_str( $args, $parsed_args );
+			if ( $defaults && ( is_array( $defaults ) || is_object( $defaults ) ) ) {
+				return wp_parse_args_recursive( $parsed_args, $defaults, $preserve_type, $preserve_integer_keys );
+			}
+			return $parsed_args;
+		}
+
 		foreach ( array( $defaults, $args ) as $list ) {
-			foreach ( (array) $list as $key => $value ) {
+			$parsed_args = array();
+			if ( is_array( $list ) ) {
+				$parsed_args =& $args;
+			} elseif ( is_object( $list ) ) {
+				$parsed_args = get_object_vars( $list );
+			}
+			foreach ( $parsed_args as $key => $value ) {
 				if ( is_integer( $key ) && ! $preserve_integer_keys ) {
 					$output[] = $value;
 				} elseif (
@@ -39,6 +56,9 @@ if ( ! function_exists( 'wp_parse_args_recursive' ) ) {
 				}
 			}
 		}
-		return ( $preserve_type && ( is_object( $args ) || is_object( $defaults ) ) ) ? (object) $output : $output;
+		if ( $preserve_type && ( is_object( $args ) || is_object( $defaults ) ) ) {
+			$output = (object) $output;
+		}
+		return $output;
 	}
 }
