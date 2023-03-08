a95bdf066833fbd236beffed508ced4f2a7e7f44
kallookoo <kallookoo@users.noreply.github.com>
2023-03-08 19:27:42 +0100

    Now the defaults can be a string

--- src/wp-parse-args-recursive.php
+++ src/wp-parse-args-recursive.php
@@ -5,7 +5,7 @@
  *
  * @author Sergio (kallookoo) <kallookoo@gmail.com>
  * @license https://www.gnu.org/licenses/gpl-2.0.html GPL2 or later
- * @version 1.3.0
+ * @version 1.4.0
  *
  * @package dsergio\com\WordPress\Helpers
  */
@@ -16,7 +16,7 @@ if ( ! function_exists( 'wp_parse_args_recursive' ) ) {
 	 * By default converts the returned type based on the $args and $defaults
 	 *
 	 * @param  string|array|object $args                   Values to merge with $defaults.
-	 * @param  array|object        $defaults               Optional. Array or Object that serves as the defaults.
+	 * @param  string|array|object $defaults               Optional. Values that serves as the defaults.
 	 *                                                     Default empty array.
 	 * @param  boolean             $preserve_type          Optional. Convert output array into object if $args or $defaults if it is.
 	 *                                                     Default true.
@@ -27,23 +27,17 @@ if ( ! function_exists( 'wp_parse_args_recursive' ) ) {
 	 */
 	function wp_parse_args_recursive( $args, $defaults = array(), $preserve_type = true, $preserve_integer_keys = false ) {
 		$output = array();
-		if ( is_string( $args ) ) {
-			parse_str( $args, $parsed_args );
-			if ( $defaults && ( is_array( $defaults ) || is_object( $defaults ) ) ) {
-				return wp_parse_args_recursive( $parsed_args, $defaults, $preserve_type, $preserve_integer_keys );
-			}
-			return $parsed_args;
-		}
-
 		foreach ( array( $defaults, $args ) as $list ) {
 			$parsed_args = array();
 			if ( is_array( $list ) ) {
-				$parsed_args =& $args;
+				$parsed_args = $list;
 			} elseif ( is_object( $list ) ) {
 				$parsed_args = get_object_vars( $list );
+			} elseif ( is_string( $list ) ) {
+				parse_str( $list, $parsed_args );
 			}
 			foreach ( $parsed_args as $key => $value ) {
-				if ( is_integer( $key ) && ! $preserve_integer_keys ) {
+				if ( is_int( $key ) && ! $preserve_integer_keys ) {
 					$output[] = $value;
 				} elseif (
 					isset( $output[ $key ] ) &&
